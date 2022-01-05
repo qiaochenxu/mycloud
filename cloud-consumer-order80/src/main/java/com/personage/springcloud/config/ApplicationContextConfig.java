8c42dbcc459af1d7dcdb49a02243e2704b4e1f88
乔晨旭 <1079935714@qq.com>
2022-01-05 21:29:04 +0800

    add:新增

--- cloud-consumer-order80/src/main/java/com/personage/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/personage/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,8 @@
 package com.personage.springcloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +21,9 @@ public class ApplicationContextConfig {
     @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    public IRule iRule(){
+        return new WeightedResponseTimeRule();
     }
 }
--- cloud-consumer-order80/src/main/java/com/personage/springcloud/controller/CorderController.java
+++ cloud-consumer-order80/src/main/java/com/personage/springcloud/controller/CorderController.java
@@ -3,6 +3,7 @@ package com.personage.springcloud.controller;
 import com.personage.springcloud.entities.Payment;
 import com.personage.springcloud.result.CommonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +30,33 @@ public class CorderController {
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL+"/payment/creater",payment,CommonResult.class);
     }
+
+    @GetMapping("consumer/payment/create01")
+    public CommonResult<Payment> create01(Payment payment) {
+
+        CommonResult body = null;
+        ResponseEntity<CommonResult> commonResultResponseEntity = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        if (commonResultResponseEntity.getStatusCode().is2xxSuccessful()){
+             body = commonResultResponseEntity.getBody();
+        } else {
+            return new CommonResult(444,"操作失败");
+        }
+        return body;
+    }
     @GetMapping("consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+    @GetMapping("consumer/payment/getPayment01/{id}")
+    public CommonResult<Payment> getPayment01(@PathVariable("id") Long id) {
+        CommonResult body = null;
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()){
+            body = forEntity.getBody();
+        } else {
+            return new CommonResult(444,"操作失败");
+        }
+        return body;
+
     }
 }
